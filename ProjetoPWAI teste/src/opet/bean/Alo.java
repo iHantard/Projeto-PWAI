b9a3c4394c350b9dbe959ed8084ab9d85afa2ada
aluno <aluno@172.31.1.121>
2017-03-02 12:30:09 -0300

    commitei porque sim

---
+++ ProjetoPWAI teste/src/opet/bean/Alo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package opet.bean;
+
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * @author aluno
+ *
+ */
+public class Alo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo!");
+
+	}
+
+}
