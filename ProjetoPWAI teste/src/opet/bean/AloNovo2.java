2a2cc2456d6408b59f7abfe410ac047939ecbe3e
aluno <aluno@172.31.1.121>
2017-03-02 12:43:53 -0300

    commit teste

---
+++ ProjetoPWAI teste/src/opet/bean/AloNovo2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package opet.bean;
+
+/**
+ * @author aluno
+ *
+ */
+public class AloNovo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Novo Olá Mundo");
+		
+	}
+
+}
